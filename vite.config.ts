e3673f21b75bcebfaaf6d6ab7b56837de641bca8
Fazi <faf2001f@gmail.com>
2025-04-28 03:33:37 +0500

    updated code

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1500, // Allow 1.5 MB chunk size before warning
+    chunkSizeWarningLimit: 1500,
   },
 })
